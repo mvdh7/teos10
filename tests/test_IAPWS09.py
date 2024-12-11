ae4285b2bafb6394a4194bb9b493342a0769917b
Matthew Humphreys <mvdh7@users.noreply.github.com>
2024-12-11 12:42:40 +0100

    IAPWS09 tests working

--- tests/test_IAPWS09.py
+++ tests/test_IAPWS09.py
@@ -2,13 +2,6 @@ import numpy as np
 
 import teos10
 
-# Test input values from IAPWS08 Table 8
-temperature_salt = np.array([273.15, 353, 273.15])
-pressure_salt = np.array([101_325, 101_325, 1e8]) / teos10.constants.dbar_to_Pa
-salinity = (
-    np.array([0.035_165_04, 0.1, 0.035_165_04]) / teos10.constants.salinity_to_salt
-)
-
 # Check values from IAPWS09 Table 6
 temperature_water = np.array([273.15, 273.15, 313.15])
 pressure_water = np.array([101_325, 1e8, 101_325]) / teos10.constants.dbar_to_Pa
@@ -167,66 +160,3 @@ def test_sound_speed_water():
             teos10.properties.sound_speed(t, p, gfunc=teos10.gibbs.water)
         )
     assert formatter(check_water["sound_speed"]) == formatter(test_values)
-
-
-# def test_gibbs_salt():
-#     """Compare saline component of Gibbs energy with check values from IAPWS08."""
-#     check_values = np.array(
-#         [
-#             -0.101_342_742 * 10**3,
-#             +0.150_871_740 * 10**5,
-#             -0.260_093_051 * 10**4,
-#         ]
-#     )
-#     test_values = []
-#     for t, p, s in zip(temperature_salt, pressure_salt, salinity):
-#         test_values.append(teos10.gibbs.salt(t, p, s))
-#     assert formatter(check_values) == formatter(test_values)
-
-
-# def test_density_seawater():
-#     check_values = [
-#         0.102_810_720e4,
-#         0.102_985_888e4,
-#         0.107_092_645e4,
-#     ]
-#     test_values = []
-#     for t, p, s in zip(temperature_salt, pressure_salt, salinity):
-#         test_values.append(
-#             teos10.properties.density(t, p, s, gfunc=teos10.gibbs.seawater)
-#         )
-#     assert formatter(check_values) == formatter(test_values)
-
-
-# def test_heat_capacity_salt():
-#     """Compare saline component of heat capacity with check values from IAPWS08."""
-#     check_values = np.array(
-#         [
-#             -0.232_959_023 * 10**3,
-#             -0.451_566_952 * 10**3,
-#             -0.133_318_225 * 10**3,
-#         ]
-#     )
-#     test_values = []
-#     for t, p, s in zip(temperature_salt, pressure_salt, salinity):
-#         test_values.append(
-#             teos10.properties.heat_capacity(t, p, s, gfunc=teos10.gibbs.salt)
-#         )
-#     assert formatter(check_values) == formatter(test_values)
-
-
-# def test_chemical_potential_water_salt():
-#     """Compare saline part of water chemical potential with check values from IAPWS08."""
-#     check_values = np.array(
-#         [
-#             -0.235_181_411 * 10**4,
-#             -0.101_085_536 * 10**5,
-#             -0.240_897_806 * 10**4,
-#         ]
-#     )
-#     test_values = []
-#     for t, p, s in zip(temperature_salt, pressure_salt, salinity):
-#         test_values.append(
-#             teos10.properties.chemical_potential_water(t, p, s, gfunc=teos10.gibbs.salt)
-#         )
-#     assert formatter(check_values) == formatter(test_values)
